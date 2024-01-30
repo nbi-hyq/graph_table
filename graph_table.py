a4caa8fd03b2671bd4081493eff4fa33165fac27
MCLoebl <79047536+MCLoebl@users.noreply.github.com>
2024-01-30 09:22:49 +0100

    correction belonging to previous commit

--- graph_table.py
+++ graph_table.py
@@ -170,6 +170,7 @@ class GraphTable:
                 for f in fusion:
                     for n in gr.nodes:
                         for m in gr.nodes:
+                            if m < n:
                                 g = copy.deepcopy(gr)
                                 measure_double_parity(g, n, m, f)
                                 self.add_orbit(g, origin=self.l_num_to_orbit[parent_orbit], link=[gr_idx, n, m, f])
